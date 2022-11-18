9ce1cf90beab94d3bd5685b8bc86a89c679749d7
Y_TEAM <81280706+azmifauzan3@users.noreply.github.com>
2022-11-18 12:29:11 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#171717'
+      },
+      screen:{
+        '2xl': '1320px'
+      }
+    },
+    },
+  },
+  plugins 
+
